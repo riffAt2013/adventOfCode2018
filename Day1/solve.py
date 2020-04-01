6d78a2637e69e240d7e3187447f408f5f45f2ae5
riffAt2013 <rifat.masud@northsouth.edu>
2020-04-02 00:12:42 +0600

    more explanation needed on the solution

---
+++ Day1/solve.py
@@ -0,0 +1,40 @@
+inputFile = open(r'C:\Users\RIFAT\Desktop\adventOfCode2018\Day1\input.txt', "r")
+lines = inputFile.readlines()
+
+def partOne():
+    result = 0
+    for line in lines:
+        result += int(line)
+
+    print(result)
+
+
+def partTwo():
+    distinct_sum = set()
+    sum = 0
+
+
+    # hacky way to repeat the loop over
+    # notice w/o the while the loop will end at the last value from
+    # the input.txt, however our problem description tells us to loop
+    # over as many times as possible.
+
+    # there might have been more overkill ways to repeat a loop over
+    # but this is the best way :)
+    while sum not in distinct_sum:
+        for line in lines:
+            distinct_sum.add(sum)
+            sum += int(line)
+            print(sum)
+
+            if sum in distinct_sum:
+                return sum
+   
+
+result = partTwo()
+print(result)
+    
+    
+    
+    
+    
